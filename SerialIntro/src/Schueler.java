eb53115cc483c413a7462d1088889c0d8c6adb8f
neukoc16 <neukoc16@new.edvhtl.at>
2018-09-28 11:56:39 +0200

    new

---
+++ SerialIntro/src/Schueler.java
@@ -0,0 +1,24 @@
+
+import java.time.LocalDate;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Konstantin
+ */
+public class Schueler {
+    private String name;
+    private LocalDate birthday;
+
+    public Schueler(String name, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+    
+    
+}
